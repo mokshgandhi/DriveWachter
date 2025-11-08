7b10743f256d815ce744decfbcd3fd497350fb19
mokshgandhi <133671985+mokshgandhi@users.noreply.github.com>
2025-11-08 22:57:20 +0530

    Implement ObjectTracker class for object tracking

--- utils/tracker.py
+++ utils/tracker.py
@@ -1,1 +1,15 @@
+from ultralytics import YOLO
+from collections import defaultdict
+
+class ObjectTracker:
+    def __init__(self):
+        self.next_id = 0
+        self.objects = defaultdict(dict)
+
+    def update(self, detections):
+        ids = []
+        for det in detections:
+            ids.append(self.next_id)
+            self.next_id += 1
+        return ids
 
