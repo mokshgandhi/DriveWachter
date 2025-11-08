21a9684051e946291523c8b3b4f164942c3496f2
mokshgandhi <133671985+mokshgandhi@users.noreply.github.com>
2025-11-08 22:55:59 +0530

    Add function to blur sensitive regions in frames

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,1 +1,10 @@
+import cv2
 
+def blur_sensitive_regions(frame, blur_model):
+    results = blur_model(frame)
+    for box in results[0].boxes.xyxy:
+        x1, y1, x2, y2 = map(int, box)
+        roi = frame[y1:y2, x1:x2]
+        roi = cv2.GaussianBlur(roi, (51, 51), 30)
+        frame[y1:y2, x1:x2] = roi
+    return frame
