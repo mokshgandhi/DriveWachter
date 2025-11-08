da42eb8baafc2850a864aa5bb022a25d59c65f2a
mokshgandhi <133671985+mokshgandhi@users.noreply.github.com>
2025-11-08 22:56:28 +0530

    Add function to get current GPS coordinates

--- utils/gps_utils.py
+++ utils/gps_utils.py
@@ -1,1 +1,8 @@
+import geocoder
 
+def get_current_gps():
+    g = geocoder.ip('me')
+    if g.latlng:
+        return {'latitude': g.latlng[0], 'longitude': g.latlng[1]}
+    else:
+        return {'latitude': None, 'longitude': None}
